4974680ce8ff383a88ea2b07c26ff24e76e67a9c
Luís <luisf.csdev@gmail.com>
2023-11-16 11:42:01 -0300

    feat: built sidebar styles

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Header } from './components/Header'
+import { Post } from './Post'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
+import './global.css'
+
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Luís Felipe" content="This is a cool post" />
+          <Post author="Vivian Grace" content="Hi there!" />
+        </main>
+
+      </div>
+    </div>
+
+  )
+}
